be93d3d5ce7232555053f40ad5f76e18af802724
gingerbreadz <anatole.lefort@gmail.com>
2016-06-08 10:52:50 +0200

    added toggle cam

--- src/nav.cpp
+++ src/nav.cpp
@@ -1,78 +1,131 @@
 #include "ros/ros.h"
-#include <ros/console.h>
 #include "std_msgs/Empty.h"
 #include "geometry_msgs/Twist.h"
 #include <geometry_msgs/Vector3.h>
 #include <ardrone_autonomy/Navdata.h>
-#include <ar_track_alvar_msgs/AlvarMarker.h>
-#include <ar_track_alvar_msgs/AlvarMarkers.h>
-#include <thread>
+#include <ardrone_autonomy/ardrone_driver.h>
+#include <aruco_msgs/MarkerArray.h>
+#include <geometry_msgs/PoseWithCovariance.h>
+#include <boost/thread.hpp>
+#include <boost/chrono.hpp>
+#include <boost/lexical_cast.hpp>
 #include <sstream> 
 #include <string>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int markerid = -1;
+int previousmarkerid = -2;
+geometry_msgs::PoseWithCovariance markerpose;
+
+std::string FloatToString(float n)
+{
+	//std::string s = boost::lexical_cast<std::string>(n);	
+	std::ostringstream ss;
+	ss << n;
+	return (ss.str());	
+}
 
 void Sleepfor(int ms)
 {
-	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+	 boost::this_thread::sleep_for(boost::chrono::milliseconds(ms));
+}
+ void chatterCallback(const aruco_msgs::MarkerArray::ConstPtr msg)
+{
+     	
+     aruco_msgs::MarkerArray lastMsg = *msg;
+     aruco_msgs::Marker lastMarker = lastMsg.markers[0];
+      
+     previousmarkerid = markerid;
+     markerid = lastMarker.id;
+     markerpose = lastMarker.pose;
+     
+     ROS_INFO("I heard: %i]",markerid);
+     //ROS_INFO("I heard: [%s]", FloatToString(msg->rotZ).c_str());
+     //std::string X, Y, Z; 
+     //X = FloatToString(x); 
+     //Y = FloatToString(y); 
+     //Z = FloatToString(z); 
+     //ROS_INFO("I heard: [%s,%s,%s] , [%f,%f,%f]", X.c_str(), Y.c_str(), Z.c_str(),x,y,z);
+     
+
 }
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "takeoffAndLand");
+	ros::init(argc, argv, "readmarker");
 	ros::NodeHandle n;
+	
+	ros::ServiceClient switchcam = n.serviceClient<ardrone_autonomy::ardrone_driver>("ardrone/setcamchannel");	
+	switchcam.call(0);
+
+	ros::Subscriber mark_sub = n.subscribe("/aruco_marker_publisher/markers", 1,chatterCallback);
 	
 	ros::Publisher takeoff_pub = n.advertise<std_msgs::Empty>(n.resolveName("ardrone/takeoff"),1,true);
 	ros::Publisher land_pub = n.advertise<std_msgs::Empty>(n.resolveName("ardrone/land"),1,true);
 	std_msgs::Empty emp_msg;
-	
+
 	ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-	geometry_msgs::Twist twist_msg;
+	geometry_msgs::Twist twist_msg;	
 
-	while (ros::ok())
-  	{
-    		takeoff_pub.publish(emp_msg);
+	start:
 
-		Sleepfor(5000);
-
-		twist_msg.angular.z=0;
-		twist_msg.linear.x=0.1;    
-                twist_pub.publish(twist_msg);
-                Sleepfor(1000);
-
-		twist_msg.linear.x=0;
-		twist_msg.angular.z=0.5;
-		twist_pub.publish(twist_msg);
-		Sleepfor(2000);
-
-		twist_msg.angular.z=0;
-		twist_msg.linear.x=0.1;
-		twist_pub.publish(twist_msg);
-		Sleepfor(1000);
-
-		twist_msg.angular.x=0;
-		twist_msg.angular.z=0.5;
-                twist_pub.publish(twist_msg);
-                Sleepfor(2000); 
-
-		twist_msg.angular.z=0;
-		twist_msg.linear.x=0.1;    
-                twist_pub.publish(twist_msg);
-                Sleepfor(1000);
-
-		twist_msg.linear.x=0;
-		twist_msg.angular.z=0.5; 
-                twist_pub.publish(twist_msg);
-                Sleepfor(2000); 
-
-		twist_msg.angular.z=0;
-		twist_msg.linear.x=0.1;    
-                twist_pub.publish(twist_msg);
-                Sleepfor(1000);
-
-		land_pub.publish(emp_msg);
-
-		ros::Duration(2).sleep();
-
+	ROS_INFO("Looking for a marker");
+	markerid = -1;
+	while(markerid != previousmarkerid)
+	{
 		ros::spinOnce();
 	}
+	
+	ROS_INFO("Marker found");
+
+	switch(markerid)
+	{
+		case 0:
+			ROS_INFO("Taking off");
+			takeoff_pub.publish(emp_msg);
+			switchcam.call(1);
+			goto start;
+
+		case 1:
+			ROS_INFO("Going Forward"); 
+			twist_msg.linear.x = 0.1;
+			twist_pub.publish(twist_msg);
+			ros::Duration(1).sleep();;
+			twist_msg.linear.x = 0;
+			twist_pub.publish(twist_msg);
+			goto start;
+
+		case 2:
+			ROS_INFO("Turning counter clockwise");
+			twist_msg.angular.z = 0.5;
+			twist_pub.publish(twist_msg);
+			ros::Duration(2).sleep();;
+			twist_msg.angular.z = 0;
+			twist_pub.publish(twist_msg);
+			goto start;
+		case 3:
+			ROS_INFO("Turning clockwise");
+			twist_msg.angular.z = -0.5;
+			twist_pub.publish(twist_msg);
+			ros::Duration(2).sleep();
+			twist_msg.angular.z = 0;
+			twist_pub.publish(twist_msg);
+			goto start;
+
+		case 4:
+			ROS_INFO("Landing");
+			land_pub.publish(emp_msg);
+			goto end;
+	}
+	
+	ROS_INFO("Out of switch");
+
+	end:
+
+	ROS_INFO("Exiting program");
+
 	return 0;
 }
